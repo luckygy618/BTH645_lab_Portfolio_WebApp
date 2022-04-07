88186475b5d372f678e7715999947a31b8a6d6a3
luckygy618 <luckygy618@gmail.com>
2022-04-07 15:20:51 -0400

    A3 added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+let moon;
+function preload() {
+  moon = loadImage("image/moon.jpg"); //load moon image
+}
 function setup() {
   //set up the canvas
   var mycanvas = createCanvas(250, 400);
-
   //insert the canvas to the HTML tag
   mycanvas.parent("p5canvas");
   background(255, 255, 255); //set up background color
@@ -11,6 +14,7 @@ function setup() {
 
 function draw() {
   background(255, 255, 255); //set up background color for every loop
+  image(moon, 1, 1, 250, 220);
   drawItem(x, y); //draw the rocket
   let s = "This is a rocket flying to the sky.";
   fill(50);
@@ -25,16 +29,13 @@ function draw() {
 function drawItem(x, y) {
   fill(255, 255, 255); //set wings to white
   arc(x, y + 40, 50, 40, PI, 0, CHORD); // ellipse(x, y, 30, 80); //draw wings
-
   fill(255, 0, 0); //set the rocket's body to red
   rect(x - 10, y - 40, 20, 80); //draw the body of rocket
   triangle(x - 10, y - 40, x, y - 50, x + 10, y - 40); //draw the top of rocket
-
   fill(255, 255, 255); //set windows to white
   square(x - 5, y, 10, 1, 1, 1, 1); //draw 3 windows
   square(x - 5, y - 20, 10, 1, 1, 1, 1);
   square(x - 5, y + 20, 10, 1, 1, 1, 1);
-
   fill(255, 255, 0); //set fire to yellow
   rect(x - 2.5, y + 50, 5, 25); //draw 3 fire lines
   rect(x - 2.5 - 10, y + 50, 5, 25);
--- sketch_image.js
+++ sketch_image.js
@@ -1,9 +1,12 @@
-let boat1, boat2, explosion;
+let boat1, boat2, explosion, sound;
+var button,
+  flag = 0;
 
 function preload() {
   boat1 = loadImage("image/boat.png"); //load boat image
   boat2 = loadImage("image/boat.png"); //load boat image
   explosion = loadImage("image/explosion.png"); //load explosion image
+  sound = loadSound("sound/sound.mp3"); //load explosion sound effect
 }
 
 function setup() {
@@ -13,31 +16,33 @@ function setup() {
   x = 120; //set up coordinate x
   y = 500; //set up coordinate y
   frameRate(120); //set up frameRate
+  button = createButton("Click Me to Fire Missile");
+  button.position(140, 400); //set the button position
+  button.mousePressed(fire); //add event listener to the button
+  noLoop(); //stop the draw() function
+  textFont("Georgia", 15); //set font type and size
+  textStyle(BOLD); //set font to bold
 }
 
 function draw() {
   background(255, 255, 255); //set up background color for every loop
-  image(boat1, 0, 1, 250, 250); //draw the boat
-  image(boat1, 0, 400, 250, 250); //draw the boat
-  drawMissile(x, y); //draw the missle
-  let s = "This is a missile";
-  fill(50);
-  text(s, 75, y, 100, 50); //show text description of missile
-  let str = "This is a missile destroyer.";
-  fill(50);
-  text(str, 50, 570, 150, 50); //show text description of bottom boat
-  let str2 = "This is the target destroyer.";
-  fill(50);
-  text(str2, 50, 80, 150, 50); //show text description of top boat
-  y = y - 8; //move the missle
-  if (y < 170) {
+  drawBoat(); //draw the boats
+  if (flag != 0) {
+    drawMissile(x, y); //draw the missle
+  }
+  flag++;
+  y = y - 5.5; //move the missle
+  if (y < 150) {
+    flag = 1;
     y = 500; //reset the missle
     boom(); //display explosion effect
-    noLoop(); //stop looping
     //freeze the draw() to show the explosion longer then continue looping
     setTimeout(function () {
-      loop(); //start looping
-    }, 1000);
+      background(255, 255, 255);
+      drawBoat();
+      button.show();
+    }, 5000);
+    noLoop();
   }
 }
 
@@ -46,10 +51,34 @@ function boom() {
   image(explosion, 0, 1, 250, 250); //display explosion image
   let s = "Boom!"; //show boom text
   fill(50);
-  text(s, 120, 125, 150, 50); //display boom text
+  text(s, 110, 125, 150, 50); //display boom text
+}
+
+function fire() {
+  loop(); //start looping
+  button.hide(); //hide the button to prevent the user to click it
+  sound.play(); //start sound
+}
+
+function drawBoat() {
+  image(boat1, 0, 1, 250, 250); //draw the boat
+  image(boat1, 0, 400, 250, 250); //draw the boat
+  let str = "This is a missile destroyer.";
+  fill(50);
+  text(str, 20, 570, 500, 100); //show text description of bottom boat
+  let str2 = "This is the target destroyer.";
+  fill(50);
+  text(str2, 20, 80, 500, 100); //show text description of top boat
 }
 
 function drawMissile(x, y) {
+  let str2 = "Wait for the explosion ends";
+  fill(50);
+  text(str2, 20, 350, 500, 100); //show text description of missile
+
+  let s = "This is a missile";
+  fill(50);
+  text(s, 55, y, 500, 50); //show text description of missile
   fill(255, 255, 255); //set wings to white
   arc(x, y, 40, 30, PI, 0, CHORD); //draw wings
   fill(255, 0, 0); //set the missle's body to red
